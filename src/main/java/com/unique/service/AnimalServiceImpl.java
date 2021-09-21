879fd379faf695c80f7706b597bca780d8f615b7
Prerit Sarvaiya <sarvaiyaprerit123@gmail.com>
2021-09-21 15:50:30 +0530

    Changes from Prerit

--- src/main/java/com/unique/service/AnimalServiceImpl.java
+++ src/main/java/com/unique/service/AnimalServiceImpl.java
@@ -55,9 +55,10 @@ public class AnimalServiceImpl implements AnimalsCrudService {
 //		return repo.findAll();
 //	}
 
-	public Animal crudOperations(Animal animals, int id, Byte flag) {
+	@Override
+	public Animal CrudAnimals(Animal animals, int id, Boolean flag) {
 		try {
-			if (id != 0 && flag == 0) {
+			if (id != 0 && flag == false) {
 				if (repo.existsById(id) == true) {
 					Animal exist = repo.findById(animals.getId()).orElse(null);
 					exist.setName(animals.getName());
@@ -77,105 +78,25 @@ public class AnimalServiceImpl implements AnimalsCrudService {
 		} catch (Exception ex) {
 			return null;
 		}
+
 	}
 
-//@Override
-//public Animal getAnimalsId(int id) {
-//	// TODO Auto-generated method stub
-//	return null;
-//}
-//@Override
-//public Animal inSert(Animal animals) {
-//	// TODO Auto-generated method stub
-//	return null;
-//}
-//@Override
-//public List<Animal> getAnimals() {
-//	// TODO Auto-generated method stub
-//	return null;
-//}
-//@Override
-//public void upDate(int id, Animal animal) {
-//	// TODO Auto-generated method stub
-//	
-//}
-//@Override
-//public void deleteID(int id) {
-//	// TODO Auto-generated method stub
-//	
-//}
-
-@Override
-public Animal CrudAnimals(Animal animals, int id, Boolean flag) {
-	try {
-		if (id != 0 && flag ==false) {
-			if (repo.existsById(id) == true) {
-				Animal exist = repo.findById(animals.getId()).orElse(null);
-				exist.setName(animals.getName());
-				exist.setPetName(animals.getPetName());
-				exist.setHabbit(animals.getHabbit());
-				exist.setPrice(animals.getPrice());
-				return repo.save(exist);
+	@Override
+	public List<Animal> CrudMultipleAnimals(List<Animal> animals, Integer id, Boolean flag) {
+		try {
+			if (id != 0 && flag == false) {
+				if (repo.existsById(id) == true) {
+					return repo.saveAll(animals);
+				} else {
+					return repo.saveAll(animals);
+				}
 			} else {
-				return repo.save(animals);
+				repo.deleteAllInBatch(animals);
 			}
-		} else {
-			repo.deleteById(id);
+			return null;
+		} catch (Exception ex) {
+			return null;
 		}
-
-		return null;
-
-	} catch (Exception ex) {
-		return null;
 	}
-	 
-}
 
-@Override
-public List<Animal> CrudMultipleAnimals(List<Animal> animals, Integer id, Boolean flag) {
-	try {
-		if(id!=0&&flag==false) {
-			if(repo.existsById(id)==true) {
-				return repo.saveAll(animals);
-			}
-			else {
-				return repo.saveAll(animals);
-			}
-		}else {
-			 repo.deleteAllInBatch(animals);
-		}
-	return null;
-	}
-	catch(Exception ex){
-		return null;
-	}
 }
-
-
-	
-}
-//@RequestMapping(value = "/reptime/api/orders/{repGroupID}/{orderID}/detail/save", method = RequestMethod.POST)
-//@JsonView(RepGroupMaintenanceDetailView.Summary.class)
-//public ResponseEntity<OrderDetail> saveOrderDetailForRepGroup(@PathVariable("repGroupID") String repGroupID,
-//@PathVariable("orderID") Long orderID, @RequestBody OrderDetail orderDetail, @RequestParam String manufacturerID,
-//@RequestHeader(value = "Origin", name = "Origin", required = false) String origin)
-//{
-//OrderDetail savedDetail = orderService.createOrUpdateOrderDetail(repGroupID, orderID, orderDetail, manufacturerID, origin);
-//if (savedDetail != null)
-//{
-//return new ResponseEntity<>(savedDetail, HttpStatus.OK);
-//} else
-//{
-//return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//}
-//}
-//@Override
-//public Animal getAnimal(Animal animals) {
-//	// TODO Auto-generated method stub
-//	return null;
-//}
-//@Override
-//public Animal getAnimalsId(int id) {
-//	// TODO Auto-generated method stub
-//	return null;
-//}
